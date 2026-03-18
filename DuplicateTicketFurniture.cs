0af6c5d16110d5f9a7ff99470874160cbd2ffd67
Sakura <sakura.kitsurugi@proton.me>
2026-03-18 12:07:04 +0200

    first commit

---
+++ DuplicateTicketFurniture.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Mod
+{
+    [BepInPlugin("sakura.elin.DuplicateTicketFurniture", "Duplicate Ticket Furniture", "0.2")]
+    public class Main : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony harmony = new Harmony("sakura.elin.DuplicateTicketFurniture");
+            harmony.PatchAll();
+        }
+    }
+}
+
+namespace Patcher
+{
+    [HarmonyPatch]
+    public class Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(typeof(TraitTicketFurniture).GetNestedType("<>c__DisplayClass5_1", BindingFlags.NonPublic),
+                "<TrySetHeldAct>b__1");
+        }
+        
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchEndForward(
+                    new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(Chara), nameof(Chara.Pick))))
+                .ThrowIfInvalid("failed to find Chara.Pick")
+                .SetInstructionAndAdvance(
+                    Transpilers.EmitDelegate(Delegate))
+                .InstructionEnumeration();
+        }
+
+        private static Thing Delegate(Chara pc, Thing thingy, bool msg = true, bool stack = true)
+        {
+            var dupe = thingy.Duplicate(1);
+            pc.Pick(dupe);
+            thingy.isNPCProperty = true;
+            if (thingy.trait is TraitPillow)
+                thingy.noSell = false;
+            return thingy;
+        }
+    }
+}
+
+
